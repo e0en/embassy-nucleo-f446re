a940703a57c6abcef894cefa29b27e5bf309f782
Yoonseop Kang <e0engoon@gmail.com>
2025-06-12 15:16:41 +0900

    Blink for STM32 NUCLEO-F446RE

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use {defmt_rtt as _, panic_probe as _};
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::{Duration, Timer};
+
+#[embassy_executor::task]
+async fn blinker(mut led: Output<'static>, interval: Duration) {
+    loop {
+        led.set_high();
+        Timer::after(interval).await;
+        led.set_low();
+        Timer::after(interval).await;
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+
+    let led = Output::new(p.PA5, Level::Low, Speed::Medium);
+    unwrap!(spawner.spawn(blinker(led, Duration::from_millis(300))));
+}
