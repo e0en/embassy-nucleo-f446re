6511a68756ee46ee14cf57efc5e438e37182b8e6
Yoonseop Kang <e0engoon@gmail.com>
2025-06-14 14:34:36 +0900

    Add skeleton for CAN control

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 mod as5600;
+mod can;
 mod clock;
 mod foc;
 mod i2c;
@@ -11,16 +12,16 @@ use {defmt_rtt as _, panic_probe as _};
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_stm32::can as stm32_can;
 use embassy_stm32::i2c as stm32_i2c;
 use embassy_stm32::{
     Config, bind_interrupts,
-    gpio::OutputType,
     i2c::{Error, I2c, Master},
     peripherals,
     timer::{low_level, simple_pwm::PwmPin},
 };
 
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::gpio::{Input, Level, Output, OutputType, Pull, Speed};
 use embassy_stm32::{peripherals::TIM1, time::khz};
 use embassy_time::{Duration, Timer};
 
@@ -91,20 +92,35 @@ async fn pwm_task(pwm_timer: low_level::Timer<'static, TIM1>) {
     }
 }
 
+#[embassy_executor::task]
+async fn can_task() {
+    // Placeholder for CAN task implementation
+    loop {
+        Timer::after(Duration::from_secs(1)).await;
+        info!("CAN task running");
+    }
+}
+
 bind_interrupts!(
     struct Irqs {
         I2C1_EV => stm32_i2c::EventInterruptHandler<peripherals::I2C1>;
         I2C1_ER => stm32_i2c::ErrorInterruptHandler<peripherals::I2C1>;
+        CAN1_RX0 => stm32_can::Rx0InterruptHandler<peripherals::CAN1>;
+        CAN1_RX1 => stm32_can::Rx1InterruptHandler<peripherals::CAN1>;
+        CAN1_SCE => stm32_can::SceInterruptHandler<peripherals::CAN1>;
+        CAN1_TX => stm32_can::TxInterruptHandler<peripherals::CAN1>;
 });
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let mut config = Config::default();
     clock::set_clock(&mut config);
-    let p = embassy_stm32::init(config);
+    let mut p = embassy_stm32::init(config);
     clock::print_clock_info(&p.RCC);
 
     let led = Output::new(p.PA5, Level::Low, Speed::Medium);
+
+    // Initialize I2c
     let i2c_config = embassy_stm32::i2c::Config::default();
     let p_i2c = I2c::new(
         p.I2C1,
@@ -117,6 +133,7 @@ async fn main(spawner: Spawner) {
         i2c_config,
     );
 
+    // Initialize PWM
     let _pin1 = PwmPin::new_ch1(p.PA8, OutputType::PushPull);
     let _pin2 = PwmPin::new_ch2(p.PA9, OutputType::PushPull);
     let _pin3 = PwmPin::new_ch3(p.PA10, OutputType::PushPull);
@@ -126,7 +143,27 @@ async fn main(spawner: Spawner) {
     timer.set_counting_mode(low_level::CountingMode::CenterAlignedUpInterrupts);
     info!("Timer frequency: {}", timer.get_frequency());
 
+    // Initialize CAN
+
+    // Pull up PA11 for CAN loopback test
+    let rx_pin = Input::new(p.PA11.reborrow(), Pull::Up);
+    core::mem::forget(rx_pin);
+
+    let mut can = stm32_can::Can::new(p.CAN1, p.PA11, p.PA12, Irqs);
+    can.modify_filters().enable_bank(
+        0,
+        stm32_can::Fifo::Fifo0,
+        stm32_can::filter::Mask32::accept_all(),
+    );
+
+    can.modify_config()
+        .set_loopback(true) // Receive own frames
+        .set_silent(true)
+        .set_bitrate(1_000_000);
+    can.enable().await;
+
     unwrap!(spawner.spawn(blinker(led, Duration::from_millis(300))));
     unwrap!(spawner.spawn(i2c_task(p_i2c)));
     unwrap!(spawner.spawn(pwm_task(timer)));
+    unwrap!(spawner.spawn(can_task()));
 }
