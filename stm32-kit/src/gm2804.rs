0a5b4b434fef5b27721068e686107ec6e56fb4bb
Yoonseop Kang <e0engoon@gmail.com>
2025-10-30 13:46:09 +0900

    Add GM2804 motor PID gains

--- stm32-kit/src/gm2804.rs
+++ stm32-kit/src/gm2804.rs
@@ -7,21 +7,21 @@ pub const SETUP: foc::controller::MotorSetup = foc::controller::MotorSetup {
 
 #[allow(dead_code)]
 pub const CURRENT_PID: foc::pid::PID = foc::pid::PID {
-    p: 0.0,
-    i: 0.0,
+    p: 5.0,
+    i: 1000.0,
     d: 0.0,
 };
 
 #[allow(dead_code)]
 pub const ANGLE_PID: foc::pid::PID = foc::pid::PID {
-    p: 4.0,
+    p: 16.0,
     i: 0.0,
     d: 0.0,
 };
 
 #[allow(dead_code)]
 pub const VELOCITY_PID: foc::pid::PID = foc::pid::PID {
-    p: 0.4,
-    i: 1.6,
+    p: 0.01,
+    i: 0.004,
     d: 0.0,
 };
