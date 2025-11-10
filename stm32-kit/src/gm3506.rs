2bffb19f10722aecfb7c64633a91a4a9dc97e337
Yoonseop Kang <e0engoon@gmail.com>
2025-11-10 16:07:25 +0900

    Update PID gains for GM3506 motor

--- stm32-kit/src/gm3506.rs
+++ stm32-kit/src/gm3506.rs
@@ -14,14 +14,14 @@ pub const CURRENT_PID: foc::pid::PID = foc::pid::PID {
 
 #[allow(dead_code)]
 pub const ANGLE_PID: foc::pid::PID = foc::pid::PID {
-    p: 16.0,
+    p: 6.0,
     i: 0.0,
     d: 0.0,
 };
 
 #[allow(dead_code)]
 pub const VELOCITY_PID: foc::pid::PID = foc::pid::PID {
-    p: 0.05,
-    i: 0.001,
+    p: 0.02,
+    i: 0.2,
     d: 0.0,
 };
